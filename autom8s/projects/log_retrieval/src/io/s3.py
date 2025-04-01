2d40545cf98ee2fb5b32cdbcc5489b117af97224
Stefan Dalecki <daleckidev@gmail.com>
2025-04-01 14:06:50 -0600

    now I understand how the s3 connection is made

--- autom8s/projects/log_retrieval/src/io/s3.py
+++ autom8s/projects/log_retrieval/src/io/s3.py
@@ -1,43 +1,43 @@
 from dataclasses import InitVar, dataclass
-from functools import partial
 
 import boto3
 
 
 @dataclass
 class S3Connection:
-    aws_access_id: str
-    aws_secret_key: str
-    aws_session_token: str
+    client: str
     starting_bucket: InitVar[str | None] = None
 
-    def __post_init__(self, starting_bucket: str | None):
-        self._authorized_client = partial(
-            boto3.client,
-            aws_access_key_id=self.aws_access_id,
-            aws_secret_access_key=self.aws_secret_key,
-            aws_session_token=self.aws_session_token,
+    @classmethod
+    def from_auth(
+        cls,
+        aws_access_id: str,
+        aws_secret_key: str,
+        aws_session_token: str | None = None,
+    ):
+        return cls(
+            client=boto3.client(
+                "s3",
+                aws_access_key_id=aws_access_id,
+                aws_secret_access_key=aws_secret_key,
+                aws_session_token=aws_session_token,
+            ),
+            starting_bucket=None,
         )
+
+    def __post_init__(self, starting_bucket: str | None):
         self.bucket = starting_bucket
 
     @property
     def bucket(self):
+        if not self._bucket:
+            raise ValueError("Bucket not set")
         return self._bucket
 
     @bucket.setter
     def bucket(self, bucket: str):
         self._bucket = bucket
 
-    @property
-    def client(self):
-        """Return an authorized S3 client for a given Bucket
-
-        If no bucket is set, raise ValueError
-        """
-        if not self.bucket:
-            raise ValueError("Bucket not set")
-        return self._authorized_client(self.bucket)
-
     def upload(self, file_path: str, object_name: str) -> None:
         """Upload a file or group of files to S3 bucket"""
-        self.client.upload_file(file_path, self.bucket, object_name)
+        self.client.upload_fileobj(file_path, self.bucket, object_name)
